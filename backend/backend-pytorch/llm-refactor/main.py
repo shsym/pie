aeac26562f242de8136f22218ecb6bc8133305b8
In Gim <in.gim@yale.edu>
2025-02-03 19:23:43 -0800

    ckpt

---
+++ backend/backend-pytorch/llm-refactor/main.py
@@ -0,0 +1,149 @@
+import torch
+from transformers import AutoProcessor, QuantoConfig, GPTQConfig, TorchAoConfig, AutoTokenizer
+from qwen_utils import process_vision_info
+
+from qwen import Qwen2_5_VLForConditionalGeneration
+from l4ma import AttentionBuffer
+
+
+# @torch.inference_mode()
+def main(model):
+    # default processor
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+    # The default range for the number of visual tokens per image in the model is 4-16384.
+    # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
+    # min_pixels = 256*28*28
+    # max_pixels = 1280*28*28
+    # processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": "./zebra.jpeg",
+                },
+                {"type": "text", "text": "Describe this image."},
+            ],
+
+        }
+    ]
+    # Preparation for inference
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+
+    print(text)
+    device = "cuda:0"
+
+    buffer = AttentionBuffer(
+        num_batch=1,
+        capacity=1024,
+        num_layers=model.config.num_hidden_layers,
+        num_heads=model.config.num_key_value_heads,
+        head_dim=model.config.hidden_size // model.config.num_attention_heads,
+        dtype=torch.bfloat16,
+        device=device
+    )
+
+    image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+
+    inputs = inputs.to(model.device)
+
+    max_new_tokens = 30
+    logits = None
+    past_key_values = None
+
+    token = 0
+    output_ids = []
+
+    buffer.clear()
+
+    # input_ids = inputs.input_ids
+    # attention_mask = inputs.attention_mask
+    # pixel_values = inputs.pixel_values
+    # image_grid_thw = inputs.image_grid_thw
+
+    for i in range(max_new_tokens):
+
+        # prefill
+        if i == 0:
+            # input_ids: torch.LongTensor = None,
+            # attention_mask: Optional[torch.Tensor] = None,
+            # position_ids: Optional[torch.LongTensor] = None,
+            # past_key_values: Optional[List[torch.FloatTensor]] = None,
+            # inputs_embeds: Optional[torch.FloatTensor] = None,
+            # use_cache: Optional[bool] = None,
+            # output_attentions: Optional[bool] = None,
+            # output_hidden_states: Optional[bool] = None,
+            # return_dict: Optional[bool] = None,
+            # pixel_values: Optional[torch.Tensor] = None,
+            # pixel_values_videos: Optional[torch.FloatTensor] = None,
+            # image_grid_thw: Optional[torch.LongTensor] = None,
+            # video_grid_thw: Optional[torch.LongTensor] = None,
+            # rope_deltas: Optional[torch.LongTensor] = None,
+            # cache_position: Optional[torch.LongTensor] = None,
+            # second_per_grid_ts: Optional[torch.Tensor] = None,
+
+            output = model(
+                input_ids=inputs.input_ids,
+                attention_mask=inputs.attention_mask,
+                pixel_values=inputs.pixel_values,
+                image_grid_thw=inputs.image_grid_thw
+            )
+            logits = output.logits
+            past_key_values = output.past_key_values
+
+        else:
+            output = model(
+                input_ids=torch.as_tensor([[token]], device=device),
+                past_key_values=past_key_values,
+                cache_position=torch.as_tensor([[i + len(inputs.input_ids[0]) - 1]], device=device),
+            )
+            logits = output.logits
+            past_key_values = output.past_key_values
+
+        last_token_logits = logits[0, -1, :]
+        token = int(torch.argmax(last_token_logits))
+        output_ids.append(token)
+
+        if token == tokenizer.eos_token_id:
+            break
+
+        output_text = tokenizer.decode(output_ids, skip_special_tokens=True,
+                                       spaces_between_special_tokens=False, )
+        print(output_text)
+
+        ...
+    # print(model.config)
+
+    # Inference: Generation of the output
+    # generated_ids = model.generate(**inputs, max_new_tokens=128)
+    # generated_ids_trimmed = [
+    #     out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    # ]
+    # output_text = processor.batch_decode(
+    #     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    # )
+    # print(output_text)
+
+
+if __name__ == "__main__":
+    # default: Load the model on the available device(s)
+    quantization_config = TorchAoConfig("int4_weight_only", group_size=128)
+
+    model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+        "Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="bfloat16", device_map="cuda:0", quantization_config=quantization_config,
+        attn_implementation="eager"
+    )
+
+    main(model)
