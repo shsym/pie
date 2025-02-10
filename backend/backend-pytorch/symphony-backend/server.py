8e9555afd6c56cdf8126122fcc0fad608b1e2d5b
In Gim <in.gim@yale.edu>
2025-02-09 20:07:23 -0800

    Add state management and block storage implementation in Rust

--- backend/backend-pytorch/symphony-backend/server.py
+++ backend/backend-pytorch/symphony-backend/server.py
@@ -8,7 +8,7 @@ from typing import Union
 import numpy as np
 import torch
 
-from blocks import KvBlockManager, BlockStorage, KvBlock, Address, Address, EmbeddingManager, EmbeddingStorage
+from blocks import KvBlockManager, BlockStorage, KvBlock, Address, Address, TokenEmbedManager, TokenEmbedStorage, KvBlockStorage
 
 type InstanceId = bytes
 
@@ -21,13 +21,13 @@ class TextToken:
 
 @dataclass
 class ImageToken:
-    image_url: str  # the url is used as an identifier
+    embed_addr: Address
     position_id: tuple[int, int, int]
 
 
 @dataclass
 class VideoToken:
-    video_url: str  # the url is used as an identifier
+    embed_addr: Address
     position_id: tuple[int, int, int]
 
 
@@ -40,43 +40,136 @@ Token = Union[TextToken, ImageToken, VideoToken]
 
 class CommandKind(StrEnum):
     # Block-level operations
-    ALLOCATE_BLOCK = "AllocateBlock"
-    FILL_BLOCK = "FillBlock"
-    FREE_BLOCK = "FreeBlock"
-    AVAILABLE_BLOCKS = "AvailableBlocks"
+    ALLOCATE_KV_BLOCKS = "AllocateKvBlocks"
+    DEALLOCATE_KV_BLOCKS = "FreeKvBlocks"
+    AVAILABLE_KV_BLOCKS = "AvailableKvBlocks"
+
+    # Block sharing
+    EXPORT_KV_BLOCKS = "ExportKvBlocks"
+    IMPORT_KV_BLOCKS = "ImportKvBlocks"
 
     # Token-level operations
-    COPY_TOKENS = "CopyTokens"
-    DROP_TOKENS = "DropTokens"
+    COPY_KV_BLOCK = "CopyTokens"
+    MASK_KV_BLOCK = "DropTokens"
+    FILL_KV_BLOCK = "FillKvBlock"
 
-    # Embedding-level operations
-    CREATE_IMAGE_TOKENS = "CreateImageTokens"
-    CREATE_VIDEO_TOKENS = "CreateVideoTokens"
+    # Embedding vector operations (input/output)
+    ALLOCATE_TOKEN_EMBEDS = "AllocateTokenEmbeds"
+    DEALLOCATE_TOKEN_EMBEDS = "DeallocateTokenEmbeds"
+    AVAILABLE_TOKEN_EMBEDS = "AvailableTokenEmbeds"
 
-    # C
+    #  Input embedding operations
+    EMBED_IMAGE = "EmbedImage"
+    EMBED_VIDEO = "EmbedVideo"
+
+    # Output embedding operations
     GET_NEXT_TOKEN_DIST = "GetNextTokenDist"
     GET_FEATURE_VECTOR = "GetFeatureVector"
+    DECODE = "Decode"
 
     # Add more if needed
 
 
+######## BLOCK ALLOC/DEALLOC ########
+
 @dataclass
-class AllocateBlockCmd:
+class AllocateKvBlocksCmd:
     num_blocks: int
 
 
 @dataclass
-class FillBlockCmd:
-    block_id: int
-    ctx_block_ids: list[int]
-    block_mask: list[bool]
-    tokens: list[Token]
-    retain_output_embed: bool
+class DeallocateKvBlocksCmd:
+    addr_offset: int
+    count: int
+
+
+@dataclass
+class AvailableKvBlocksCmd:
+    pass
+
+
+@dataclass
+class ExportKvBlocksCmd:
+    resource_name: str
+    addr_offset: int
+    count: int
+
+
+@dataclass
+class ImportKvBlocksCmd:
+    resource_name: str
+
+
+######## BLOCK CTRL ########
+
+@dataclass
+class FillKvBlockCmd:
+    addr: Address
+    ctx_addrs: list[Address]
+    mask: list[bool]
+
+    input_embeds: list[Token]
+    output_embeds: list[Address]
+    output_embed_offset: int
+
+
+@dataclass
+class CopyKvBlockCmd:
+    src_addr: int
+    dst_addr: int
+    src_token_offset: int
+    dst_token_offset: int
+    token_count: int
+
+
+@dataclass
+class MaskKvBlockCmd:
+    addr: int
+    token_offset: int
+    token_count: int
+
+
+######## BLOCK ALLOC/DEALLOC ########
+
+@dataclass
+class AllocateTokenEmbedsCmd:
+    num_tokens: int
+
+
+@dataclass
+class DeallocateTokenEmbedsCmd:
+    addr_offset: int
+    count: int
+
+
+@dataclass
+class AvailableTokenEmbedsCmd:
+    ...
+
+
+#### INPUT EMBEDDING ####
+
+@dataclass
+class EmbedImageCmd:
+    image_url: str
+
+
+@dataclass
+class EmbedVideoCmd:
+    video_url: str
+
+
+#### OUTPUT EMBEDDING ####
+
+
+@dataclass
+class DecodeCmd:
+    ...
 
 
 @dataclass
 class GetNextTokenDistCmd:
-    block_id: int
+    addr: int
     offset: int
     size: int
     drop_output_embed: bool
@@ -84,58 +177,27 @@ class GetNextTokenDistCmd:
 
 @dataclass
 class GetFeatureVectorCmd:
-    block_id: int
+    addr: int
     offset: int
     size: int
     drop_output_embed: bool
 
 
-@dataclass
-class CopyTokensCmd:
-    src_block_id: int
-    dst_block_id: int
-    src_offset: int
-    dst_offset: int
-    size: int
-
-
-@dataclass
-class DropTokensCmd:
-    block_id: int
-    offset: int
-    size: int
-
-
-@dataclass
-class FreeBlockCmd:
-    block_id_offset: int
-    count: int
-
-
-@dataclass
-class AvailableBlocksCmd:
-    pass
-
-
-@dataclass
-class CreateImageTokensCmd:
-    image_url: str
-
-
-@dataclass
-class CreateVideoTokensCmd:
-    video_url: str
-
-
 CommandPayload = Union[
-    AllocateBlockCmd,
-    FillBlockCmd,
-    FreeBlockCmd,
-    AvailableBlocksCmd,
-    CopyTokensCmd,
-    DropTokensCmd,
-    CreateImageTokensCmd,
-    CreateVideoTokensCmd
+    AllocateKvBlocksCmd,
+    DeallocateKvBlocksCmd,
+    AvailableKvBlocksCmd,
+    ExportKvBlocksCmd,
+    ImportKvBlocksCmd,
+    FillKvBlockCmd,
+    CopyKvBlockCmd,
+    MaskKvBlockCmd,
+    AllocateTokenEmbedsCmd,
+    DeallocateTokenEmbedsCmd,
+    AvailableTokenEmbedsCmd,
+    DecodeCmd,
+    EmbedImageCmd,
+    EmbedVideoCmd
 ]
 
 
@@ -192,70 +254,117 @@ class Response:
     payload: ResponsePayload
 
 
-
-
 # Any reusable states, blocks and embeddings
 class Resource:
-    name:str
+    owner_inst_id: InstanceId
 
-    # ones that are not in the list are denied
-    access_control: list[str]
+    block_addr: Address
+    block_offset: int
 
-    def __init__(self, name: str):
-        self.name = name
-        self.access_control = []
+    def __init__(self, owner_inst_id: InstanceId, block_addr: Address, block_offset: int):
+        self.owner_inst_id = owner_inst_id
+        self.block_addr = block_addr
+        self.block_offset = block_offset
+
+
+class Instance:
+    owned_resources: list[str]
+    usage_stats: dict[str, int]
+
+    def __init__(self):
+        self.owned_resources = []
+        self.usage_stats = {}
 
 
 class ServerState:
-
     # resource name resolution.
 
     # public resources should be visible to the users.
-    resources: dict[str, str]
+    resources: dict[str, Resource]
+    instances: dict[InstanceId, Instance]
 
     # state management
     block_manager: KvBlockManager
-    input_manager: EmbeddingManager
-    output_manager: EmbeddingManager
+    input_manager: TokenEmbedManager
+    output_manager: TokenEmbedManager
 
     # command batcher
     fill_cmd_batcher: FillBlockCmdBatcher
     img_cmd_batcher: CreateImageTokensCmdBatcher
 
-    def __init__(self, block_storage: BlockStorage, embedding_storage: EmbeddingStorage):
+    def __init__(self, block_storage: KvBlockStorage, embedding_storage: TokenEmbedStorage):
+        self.resources = {}
+        self.instances = {}
+
         self.block_manager = KvBlockManager(block_storage)
 
         # input and output managers actually share the same physical storage
-        self.input_manager = EmbeddingManager(embedding_storage)
-        self.output_manager = EmbeddingManager(embedding_storage)
-        self.fill_cmd_batcher = FillBlockCmdBatcher()
+        self.input_manager = TokenEmbedManager(embedding_storage)
+        self.output_manager = TokenEmbedManager(embedding_storage)
 
-    def allocate_blocks(self, inst_id: InstanceId, num_blocks: int) -> list[Address]:
+        # command batchers
+        self.fill_cmd_batcher = FillBlockCmdBatcher()
+        self.img_cmd_batcher = CreateImageTokensCmdBatcher()
+
+    def init_instance(self, inst_id: InstanceId):
+        self.instances[inst_id] = Instance()
+
+    def delete_instance(self, inst_id: InstanceId):
+
+        # release all the resources
+        for r in self.instances[inst_id].owned_resources:
+            del self.resources[r]
+
+        del self.instances[inst_id]
+
+    def allocate_kv_blocks(self, inst_id: InstanceId, num_blocks: int) -> list[Address]:
         new_block_ids = self.block_manager.allocate_blocks(inst_id, num_blocks)
         return new_block_ids
 
-    def fill_blocks(self, inst_id: InstanceId, block_id: Address, ctx_block_ids: list[Address], block_mask: list[list[bool]], embeddings: list[Token], retain_output_embed: bool):
+    def deallocate_kv_blocks(self, inst_id: InstanceId, addr_offset: Address, count: int):
+        inst = self.instances[inst_id]
+
+        # check if it deallocates the exported resources
+        if inst.owned_resources:
+            for r in inst.owned_resources:
+                resource = self.resources[r]
+                if addr_offset <= resource.block_addr < addr_offset + count:
+                    del self.resources[r]
+
+        # even if the other instances are using the deleted blocks,
+        # that's okay because their refcount will not be zero yet.
+        # they will be released when the refcount reaches zero.
+
+        self.block_manager.delete_blocks(inst_id, list(range(addr_offset, addr_offset + count)))
+
+    def fill_kv_block(self, inst_id: InstanceId, addr: Address, ctx_addrs: list[Address], block_mask: list[list[bool]], embeddings: list[Token], retain_output_embed: bool):
         # first validate if all the blocks are in the storage
 
         addr_space = self.block_manager.addr_space[inst_id]
 
         # translate all block ids into block ptr
         ctx_block_ptrs = []
-        for b_id in ctx_block_ids:
+        for b_id in ctx_addrs:
             b_ptr = addr_space.resolve(b_id)
             ctx_block_ptrs.append(b_ptr)
 
-        block_ptr = addr_space.resolve(block_id)
+        block_ptr = addr_space.resolve(addr)
 
         # self.cmd_batcher << accepts block pointers.
         self.fill_cmd_batcher.add
         ...
 
-    def free_blocks(self, inst_id: InstanceId, offset: Address, count: int):
-        self.block_manager.delete_blocks(inst_id, list(range(offset, offset + count)))
-
-    def available_blocks(self) -> int:
+    def available_kv_blocks(self) -> int:
         return self.block_manager.num_free_blocks()
+
+    def export_blocks(self, inst_id: InstanceId, resource_name: str, offset: Address, count: int):
+        self.resources[resource_name] = Resource(inst_id, offset, count)
+        self.instances[inst_id].owned_resources.append(resource_name)
+
+    def import_blocks(self, inst_id: InstanceId, resource_name: str):
+        resource = self.resources[resource_name]
+        src_addrs = list(range(resource.block_addr, resource.block_addr + resource.block_offset))
+        self.block_manager.allocate_linked_blocks(inst_id, resource.owner_inst_id, src_addrs)
 
     def copy_tokens(self, inst_id: InstanceId, src_block_id: Address, dst_block_id: Address, src_offset: int, dst_offset: int, size: int):
         self.block_manager.copy_tokens(inst_id, src_block_id, dst_block_id, src_offset, dst_offset, size)
@@ -293,38 +402,38 @@ def parse_incoming_message(msg: list) -> Request:
 
     # In a typical scenario, parameters is a list. We'll match on (kind, parameters).
     match kind, parameters:
-        case (CommandKind.ALLOCATE_BLOCK, [num_blocks]):
-            payload = AllocateBlockCmd(num_blocks=num_blocks)
+        case (CommandKind.ALLOCATE_KV_BLOCKS, [num_blocks]):
+            payload = AllocateKvBlocksCmd(num_blocks=num_blocks)
 
-        case (CommandKind.FREE_BLOCK, [offset, count]):
-            payload = FreeBlockCmd(block_id_offset=offset, count=count)
+        case (CommandKind.DEALLOCATE_KV_BLOCKS, [offset, count]):
+            payload = DeallocateKvBlocksCmd(addr_offset=offset, count=count)
 
-        case (CommandKind.FILL_BLOCK, [block_id, ctx_block_ids, block_mask, embeddings, retain_output_embed]):
-            payload = FillBlockCmd(block_id=block_id, ctx_block_ids=ctx_block_ids, block_mask=block_mask, tokens=embeddings, retain_output_embed=retain_output_embed)
+        case (CommandKind.FILL_KV_BLOCK, [block_id, ctx_block_ids, block_mask, embeddings, retain_output_embed]):
+            payload = FillKvBlockCmd(addr=block_id, ctx_addrs=ctx_block_ids, mask=block_mask, tokens=embeddings, retain_output_embed=retain_output_embed)
 
-        case (CommandKind.AVAILABLE_BLOCKS, []):
-            payload = AvailableBlocksCmd()
+        case (CommandKind.AVAILABLE_KV_BLOCKS, []):
+            payload = AvailableKvBlocksCmd()
 
         case (CommandKind.COPY_TOKENS, [src, dst, src_offset, dst_offset, size]):
-            payload = CopyTokensCmd(
-                src_block_id=src, dst_block_id=dst,
-                src_offset=src_offset, dst_offset=dst_offset, size=size
+            payload = CopyKvBlockCmd(
+                src_addr=src, dst_addr=dst,
+                src_token_offset=src_offset, dst_token_offset=dst_offset, size=size
             )
 
-        case (CommandKind.DROP_TOKENS, [block_id, offset, size]):
-            payload = DropTokensCmd(block_id=block_id, offset=offset, size=size)
+        case (CommandKind.MASK_KV_BLOCK, [block_id, offset, size]):
+            payload = MaskKvBlockCmd(addr=block_id, offset=offset, size=size)
 
-        case (CommandKind.CREATE_IMAGE_TOKENS, [image_url]):
-            payload = CreateImageTokensCmd(image_url=image_url)
+        case (CommandKind.EMBED_IMAGE, [image_url]):
+            payload = EmbedImageCmd(image_url=image_url)
 
-        case (CommandKind.CREATE_VIDEO_TOKENS, [video_url]):
-            payload = CreateVideoTokensCmd(video_url=video_url)
+        case (CommandKind.EMBED_VIDEO, [video_url]):
+            payload = EmbedVideoCmd(video_url=video_url)
 
         case (CommandKind.GET_NEXT_TOKEN_DIST, [block_id, offset, size, drop_output_embed]):
-            payload = GetNextTokenDistCmd(block_id=block_id, offset=offset, size=size, drop_output_embed=drop_output_embed)
+            payload = GetNextTokenDistCmd(addr=block_id, offset=offset, size=size, drop_output_embed=drop_output_embed)
 
         case (CommandKind.GET_FEATURE_VECTOR, [block_id, offset, size, drop_output_embed]):
-            payload = GetFeatureVectorCmd(block_id=block_id, offset=offset, size=size, drop_output_embed=drop_output_embed)
+            payload = GetFeatureVectorCmd(addr=block_id, offset=offset, size=size, drop_output_embed=drop_output_embed)
 
         case _:
             raise ValueError(f"Invalid parameters for command: {cmd_str}")
@@ -351,8 +460,8 @@ def handle_command(state: ServerState, req: Request) -> tuple[Response | None, b
     # this function may DENY the request if the command is not ready to be processed.
 
     match req.kind, req.payload:
-        case (CommandKind.ALLOCATE_BLOCK, AllocateBlockCmd(num_blocks)):
-            allocated_ids = state.allocate_blocks(num_blocks)
+        case (CommandKind.ALLOCATE_KV_BLOCKS, AllocateKvBlocksCmd(num_blocks)):
+            allocated_ids = state.allocate_kv_blocks(num_blocks)
             if not allocated_ids:
                 raise ValueError("Allocation returned empty")
             offset = allocated_ids[0]
@@ -363,7 +472,7 @@ def handle_command(state: ServerState, req: Request) -> tuple[Response | None, b
                 payload=AllocatedBlocksResp(block_id_offset=offset, count=count)
             ), True
 
-        case (CommandKind.FILL_BLOCK, FillBlockCmd(block_id=block_id, ctx_block_ids=ctx_block_ids, block_mask=block_mask, tokens=embeddings, retain_output_embed=retain_output_embed)):
+        case (CommandKind.FILL_KV_BLOCK, FillKvBlockCmd(addr=block_id, ctx_addrs=ctx_block_ids, mask=block_mask, tokens=embeddings, retain_output_embed=retain_output_embed)):
 
             # inspect the embedding to see if they are ready. if not, return None, False
             for b in embeddings:
@@ -390,36 +499,36 @@ def handle_command(state: ServerState, req: Request) -> tuple[Response | None, b
 
             return None, True
 
-        case (CommandKind.FREE_BLOCK, FreeBlockCmd(block_id_offset=offset, count=c)):
-            state.free_blocks(req.instance_id, offset, c)
+        case (CommandKind.DEALLOCATE_KV_BLOCKS, DeallocateKvBlocksCmd(addr_offset=offset, count=c)):
+            state.deallocate_kv_blocks(req.instance_id, offset, c)
             return None, True
 
-        case (CommandKind.AVAILABLE_BLOCKS, AvailableBlocksCmd()):
-            available = state.available_blocks()
+        case (CommandKind.AVAILABLE_KV_BLOCKS, AvailableKvBlocksCmd()):
+            available = state.available_kv_blocks()
             return Response(
                 instance_id=req.instance_id,
                 kind=ResponseKind.AVAILABLE_COUNT,
                 payload=AvailableCountResp(count=available)
             ), True
 
-        case (CommandKind.COPY_TOKENS, CopyTokensCmd(src_block_id=src, dst_block_id=dst, src_offset=src_offset, dst_offset=dst_offset, size=size)):
+        case (CommandKind.COPY_TOKENS, CopyKvBlockCmd(src_addr=src, dst_addr=dst, src_token_offset=src_offset, dst_token_offset=dst_offset, size=size)):
             state.copy_tokens(req.instance_id, src, dst, src_offset, dst_offset, size)
             return None, True
 
-        case (CommandKind.DROP_TOKENS, DropTokensCmd(block_id=b, offset=offset, size=size)):
+        case (CommandKind.MASK_KV_BLOCK, MaskKvBlockCmd(addr=b, offset=offset, size=size)):
             state.drop_tokens(req.instance_id, b, offset, size)
             return None, True
 
-        case (CommandKind.CREATE_IMAGE_TOKENS, CreateImageTokensCmd(image_url=url)):
+        case (CommandKind.EMBED_IMAGE, EmbedImageCmd(image_url=url)):
             return None, True
 
-        case (CommandKind.CREATE_VIDEO_TOKENS, CreateVideoTokensCmd(video_url=url)):
+        case (CommandKind.EMBED_VIDEO, EmbedVideoCmd(video_url=url)):
             return None, True
 
-        case (CommandKind.GET_NEXT_TOKEN_DIST, GetNextTokenDistCmd(block_id=b, offset=offset, size=size, drop_output_embed=drop_output_embed)):
+        case (CommandKind.GET_NEXT_TOKEN_DIST, GetNextTokenDistCmd(addr=b, offset=offset, size=size, drop_output_embed=drop_output_embed)):
             return None, True
 
-        case (CommandKind.GET_FEATURE_VECTOR, GetFeatureVectorCmd(block_id=b, offset=offset, size=size, drop_output_embed=drop_output_embed)):
+        case (CommandKind.GET_FEATURE_VECTOR, GetFeatureVectorCmd(addr=b, offset=offset, size=size, drop_output_embed=drop_output_embed)):
             return None, True
 
         case _:
