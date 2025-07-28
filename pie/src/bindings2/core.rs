9ae8ac8690e85c6c758528d8925c0a74bc0fe80b
In Gim <in.gim@yale.edu>
2025-07-27 20:10:54 -0700

    inferlet: allow passing cmd line arguments

--- pie/src/bindings2/core.rs
+++ pie/src/bindings2/core.rs
@@ -1,7 +1,7 @@
 use crate::instance::InstanceState;
-use crate::model::{self, Command, StreamPriority};
 use crate::messaging::{PubSubCommand, PushPullCommand, dispatch_i2i, dispatch_u2i};
-use crate::{bindings2, server};
+use crate::model::{self, Command, StreamPriority};
+use crate::{bindings2, kvs, server};
 use std::mem;
 use std::sync::atomic::{AtomicU32, Ordering};
 use tokio::sync::{mpsc, oneshot};
@@ -101,8 +101,7 @@ impl bindings2::pie::inferlet::core::Host for InstanceState {
     }
 
     async fn get_arguments(&mut self) -> anyhow::Result<Vec<String>> {
-        // Placeholder: Return empty args
-        Ok(vec![])
+        Ok(self.arguments().to_vec())
     }
 
     async fn debug_query(&mut self, _query: String) -> anyhow::Result<String> {
@@ -176,6 +175,35 @@ impl bindings2::pie::inferlet::core::Host for InstanceState {
     ) -> anyhow::Result<Vec<String>> {
         // Placeholder: Implement trait filtering logic
         Ok(model::available_models())
+    }
+
+    async fn store_set(&mut self, key: String, value: String) -> anyhow::Result<()> {
+        kvs::dispatch(kvs::Command::Set { key, value })?;
+        Ok(())
+    }
+    async fn store_get(&mut self, key: String) -> anyhow::Result<Option<String>> {
+        let (tx, rx) = oneshot::channel();
+        kvs::dispatch(kvs::Command::Get { key, response: tx })?;
+        let res = rx.await?;
+        Ok(res)
+    }
+
+    async fn store_delete(&mut self, key: String) -> anyhow::Result<()> {
+        kvs::dispatch(kvs::Command::Delete { key })?;
+        Ok(())
+    }
+
+    async fn store_exists(&mut self, key: String) -> anyhow::Result<bool> {
+        let (tx, rx) = oneshot::channel();
+        kvs::dispatch(kvs::Command::Exists { key, response: tx })?;
+        let res = rx.await?;
+        Ok(res)
+    }
+    async fn store_list_keys(&mut self) -> anyhow::Result<Vec<String>> {
+        let (tx, rx) = oneshot::channel();
+        kvs::dispatch(kvs::Command::ListKeys { response: tx })?;
+        let res = rx.await?;
+        Ok(res)
     }
 }
 
